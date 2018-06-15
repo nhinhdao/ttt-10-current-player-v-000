8df85bcee14d76eddbc4c35590b5848acbd1a0a7
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-15 13:40:41 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,10 @@ board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 
 def turn_count(board)
       count = 0
-      board.each { |token| if token != " "
-                  count += 1 end}
+      board.each do |token|
+            if token != " "
+                  count += 1
+            end
       end
       count
 end
