59e18d14f9b93e6db49440b3066a4f7f5ae1580e
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-15 13:39:26 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,7 +11,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-      turn_count(board) % 2 == 0 ? "0" : "X"
+      turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
 count = turn_count(board)
